03174366cf43ec712e5889366b8fb5c707b1e519
Mao Yunlong <thrbowl@gmail.com>
2013-04-12 23:28:08 +0800

    test the global option whether or not to cancel the ajaxSetup

--- data/ajax.py
+++ data/ajax.py
@@ -4,4 +4,9 @@ app = Bottle()
 
 @app.route('/complete/')
 def ajax():
-    return 'Ready? GO!!!'
+    return 'Ready? complete!!!'
+
+
+@app.route('/overwrite/')
+def ajax():
+    return 'Ready? overwrite!!!'
